bf1fc6675db7aa3a21a0ba2b0e1ebf96c929b3c6
jiangjunhui <2806139037@qq.com>
2024-12-05 14:42:31 +0800

    feat：全局异常信息收集

---
+++ flutter_module/lib/core/constants/constant.dart
@@ -0,0 +1,11 @@
+/**
+ * @author: jiangjunhui
+ * @date: 2024/12/5
+ */
+import 'package:flutter/foundation.dart';
+
+class Constant {
+ /// App运行在Release环境时，inProduction为true；当App运行在Debug和Profile环境时，inProduction为false
+ static const bool inProduction  = false;
+
+}
